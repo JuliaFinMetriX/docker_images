056b65e18be1e01f118b29bda0c13a1bef6638a3
Christian Groll <groll.christian.edu@gmail.com>
2015-02-05 16:36:34 +0100

    jfinm_dev added, nlopt 2.4.2

---
+++ jfinm_dev/nlopt_install.sh
@@ -0,0 +1,4 @@
+mkdir -p /home/jovyan/programs
+wget http://ab-initio.mit.edu/nlopt/nlopt-2.4.2.tar.gz -O /home/jovyan/programs/nlopt-2.4.2.tar.gz
+cd /home/jovyan/programs/
+tar xvfz /home/jovyan/programs/nlopt-2.4.2.tar.gz
